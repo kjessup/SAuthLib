e3003a7986c1d092b25230194bea8a49709f793d
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-04-23 16:18:58 -0400

    don't delete reset token when page is accessed

--- Sources/SAuthLib/SAuthHandlers.swift
+++ Sources/SAuthLib/SAuthHandlers.swift
@@ -170,8 +170,9 @@ extension SAuthHandlers {
 					try table.where(\PasswordResetToken.aliasId == addr).delete()
 					return nil
 				}
-				let newToken = try self.addPasswordResetToken(address: addr, db: db)
-				return PasswordResetToken(aliasId: addr, authId: newToken, expiration: 0)
+//				let newToken = try self.addPasswordResetToken(address: addr, db: db)
+//				return PasswordResetToken(aliasId: addr, authId: newToken, expiration: 0)
+				return resetToken
 			}
 			guard let newResetToken = newToken else {
 				throw HTTPResponseError(status: .notFound, description: "Token not found.")
